105cd725349cebe29431820cf870ce0f3d33a95e
Ben Carlson <benocarlson@gmail.com>
2020-03-19 21:29:38 -0600

    Add TaskList component

---
+++ src/components/TaskList.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="wrapper">
+    <div class="tasks">
+      <div class="task" v-for="task in tasks" :key="task.id">
+        <h1>{{task.name}}</h1>
+        <div class="task-controls">
+          <button class="complete">Mark as Complete</button>
+          <button class="edit">Edit</button>
+          <button class="delete">Delete</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TaskList',
+  props: {
+    tasks: Array
+  }
+}
+</script>
+
+<style scoped>
+.wrapper {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.tasks {
+  margin-top: 30px;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-around;
+}
+
+.task {
+  width: 27%;
+}
+
+.task h1 {
+  font-size: 15px;
+}
+
+.task-controls {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-evenly;
+  align-items: center;
+}
+
+.task-controls button {
+  margin: 5px;
+  padding: 5px;
+  font-size: 10px;
+}
+
+.task-controls .complete {
+  width: 90%;
+}
