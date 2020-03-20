32708fc321a23173d1889c1ef8d63495fe92d5b8
Ben Carlson <benocarlson@gmail.com>
2020-03-19 21:07:11 -0600

    Add header and footer

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,20 @@
 <template>
   <div id="app">
+    <div id="header">
+    <div id="logo">
+      <img alt="Taskflow logo" src="/images/Logo.png">
+    </div>
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/">Home</router-link>
+      <router-link to="/create">Create a Taskflow</router-link>
+    </div>
     </div>
     <router-view/>
+    <div id="footer">
+      <a href='https://github.com/benocarlson/taskflow-organizer-website'>
+        Visit the GitHub repository for this website
+      </a>
+    </div>
   </div>
 </template>
 
@@ -15,6 +25,19 @@
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+}
+
+#header {
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+}
+
+#logo img {
+  width: 200px;
+  height: 200px;
+  object-fit: scale-down;
+  margin: 50px;
 }
 
 #nav {
