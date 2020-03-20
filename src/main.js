f8fb1dc5c89564da2eac4d4bd4342ea09cb2be2c
Ben Carlson <benocarlson@gmail.com>
2020-03-19 21:30:27 -0600

    Apply mock data to application

--- src/main.js
+++ src/main.js
@@ -2,9 +2,12 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 
-Vue.config.productionTip = false
+import mock from './mock-data.js'
+
+let data = mock;
 
 new Vue({
   router,
+  data,
   render: h => h(App)
 }).$mount('#app')
--- src/mock-data.js
+++ src/mock-data.js
@@ -186,3 +186,5 @@ let mock = {
     }]
   }]
 }
+
+export default mock;
